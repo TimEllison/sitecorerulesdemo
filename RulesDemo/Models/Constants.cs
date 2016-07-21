a96633e59116858cac62b5ccb7add40030ea5056
TimEllison <tellison@captechconsulting.com>
2016-07-21 16:42:21 -0400

    more good stuff for the demo

--- RulesDemo/Models/Constants.cs
+++ RulesDemo/Models/Constants.cs
@@ -13,5 +13,10 @@ namespace RulesDemo.Models
         {
             public const string BannerTemplate = "{D15F2735-10A4-47F5-A31A-837E8896E5A8}";
         }
+
+        public class RenderingIds
+        {
+            public const string BetterWelcomeBanner = "{B34C689B-E56A-481D-8F54-B259AAA7F513}";
+        }
     }
 }
--- RulesDemo/Pipelines/RequestBegin/GeolocationResolver.cs
+++ RulesDemo/Pipelines/RequestBegin/GeolocationResolver.cs
@@ -5,6 +5,7 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web;
+using Glass.Mapper;
 using Sitecore.ApplicationCenter.Applications;
 using Sitecore.Mvc.Pipelines.Request.RequestBegin;
 using Sitecore.Pipelines.HttpRequest;
@@ -18,9 +19,9 @@ namespace RulesDemo.Pipelines.RequestBegin
             var states = new string[]
             {
                 "NC",
-//                "ND",
-//                "SD",
-//                "AK",
+                "ND",
+                "SD",
+                "AK",
 //                "AZ",
 //                "DC",
                 "NY",
@@ -32,13 +33,16 @@ namespace RulesDemo.Pipelines.RequestBegin
             var itemNumber = new Random().Next(0, states.Length);
             if (Sitecore.Context.Database == null || Sitecore.Context.Database.Name == "master")
             {
-                itemNumber = 2;
+                itemNumber = 5;
             }
             
             var cookie = args.Context.Request.Cookies.Get("Geolocation") ?? new HttpCookie("Geolocation");
             cookie.Expires = DateTime.UtcNow.AddMinutes(-1);
             cookie["State"] = states[itemNumber];
+            args.Context.Request.Cookies.Remove("Geolocation");
+            args.Context.Request.Cookies.Add(cookie);
             args.Context.Response.Cookies.Add(cookie);
+            Sitecore.Diagnostics.Log.Debug("GEOLOCATION::User state has been set to {0}".Formatted(states[itemNumber]));
         }
     }
 }
