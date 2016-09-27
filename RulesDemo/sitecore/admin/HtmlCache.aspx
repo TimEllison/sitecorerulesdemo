19411b6d0f725f40ce7a61a6a82795c9a93eab39
TimEllison <tellison@captechconsulting.com>
2016-09-27 09:00:49 -0400

    adding cache viewer page.

---
+++ RulesDemo/sitecore/admin/HtmlCache.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="HtmlCache.aspx.cs" Inherits="CarMax.Online.Web.sitecore.admin.HtmlCache" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <table>
+            <thead>
+            <tr>
+                <td>Rendering</td>
+                <td>Key Count</td>
+            </tr>
+            </thead>
+            <tbody id="TableBody" runat="server">
+            </tbody>
+        </table>
+    </div>
+    </form>
+</body>
+</html>
---
+++ RulesDemo/sitecore/admin/HtmlCache.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using CarMax.Online.Web.Models.Base;
+using Sitecore.Caching;
+using Sitecore.Data;
+using Sitecore.Sites;
+
+namespace CarMax.Online.Web.sitecore.admin
+{
+    public partial class HtmlCache : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var cache = CacheManager.GetHtmlCache(SiteContext.GetSite("carmax"));
+
+            var context = new Glass.Mapper.Sc.SitecoreContext(Database.GetDatabase("master"));
+            var renderings =
+                context.Query<ContentBase>(
+                    "/sitecore/layout/Renderings/Carmax//*[@@templatename='View rendering' or @@templatename='Controller rendering']");
+
+            var data = from rendering in renderings
+                let keys =
+                    cache.InnerCache.GetCacheKeys()
+                        .ToArray()
+                        .Where(x => x.ToString().ToLower().Contains(rendering.FullPath.ToLower()))
+                select new
+                {
+                    Name = rendering.FullPath,
+                    CacheCount = keys.Count()
+                };
+
+            foreach (var item in data)
+            {
+               TableBody.Controls.Add(new LiteralControl(string.Format("<tr><td>{0}</td><td>{1}</tr>", item.Name, item.CacheCount)));
+            }
+        }
+    }
+}
